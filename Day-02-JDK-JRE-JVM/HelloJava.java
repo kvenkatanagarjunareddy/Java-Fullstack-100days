8dc452c86999dbfa4fc55b5f58368c759138f71c
Kandi Venkata Nagarjuna Reddy <157882371+kvenkatanagarjunareddy@users.noreply.github.com>
2026-09-03 22:19:54 +0530

    Add HelloJava class with main method

---
+++ Day-02-JDK-JRE-JVM/HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java");
+        System.out.println("Day 2");
+        System.out.println("Learning Java Architecture");
+    }
+}
