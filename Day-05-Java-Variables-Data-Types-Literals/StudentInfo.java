69f8be1d1178c7afbb46688755018b5edcbe5120
Kandi Venkata Nagarjuna Reddy <157882371+kvenkatanagarjunareddy@users.noreply.github.com>
2026-09-06 21:30:55 +0530

    Add StudentInfo class to display student details
    This class displays student information including name, age, grade, percentage, and pass status.

---
+++ Day-05-Java-Variables-Data-Types-Literals/StudentInfo.java
@@ -0,0 +1,17 @@
+public class StudentInfo { 
+    public static void main(String[] args) { 
+        
+        String name = "Rahul"; 
+        int age = 22; 
+        char grade = 'A'; 
+        double percentage = 85.75; 
+        boolean passed = true; 
+
+        // Formatted Output Display
+        System.out.println("Student Name : " + name); 
+        System.out.println("Age          : " + age); 
+        System.out.println("Grade        : " + grade); 
+        System.out.println("Percentage   : " + percentage + "%"); 
+        System.out.println("Passed       : " + passed); 
+    } 
+}
